2278f29d7a0f0e355e179a9a2ac30d2d37540a7b
Gregory Giecold <GGiecold@users.noreply.github.com>
2017-08-11 08:06:44 -0400

    Update and rename multi-class_classification.py to multiclass_classification.py

--- src/multiclass_classification.py
+++ src/multiclass_classification.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-
+from __future__ import print_function
 from keras import layers, models
-from keras.optimizers import RMSProp
+from keras.optimizers import RMSprop
 from keras.utils import to_categorical
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,12 +18,12 @@ def vectorize(sequences, dim=10000):
   return res
 
 
-def build_model(train_labels, num_words=10000):
+def build_model(num_classes=46, num_words=10000):
   model = models.Sequential()
   
   model.add(layers.Dense(64, activation='relu', input_shape=(num_words,)))
   model.add(layers.Dense(64, activation='relu'))
-  model.add(layers.Dense(max(train_labels) + 1, activation='softmax'))
+  model.add(layers.Dense(num_classes, activation='softmax'))
   
   return model
   
@@ -42,11 +42,13 @@ def plot_loss_and_accuracy(history):
   plt.ylabel('Loss')
   
   plt.show()
-  sleep(10)
+  sleep(5)
   plt.clf()
+  sleep(1)
+  plt.close()
   
-  training_accuracies = dico['binary_acc']
-  validation_accuracies = dico['val_binary_acc']
+  training_accuracies = dico['acc']
+  validation_accuracies = dico['val_acc']
   
   plt.plot(epochs, training_accuracies, 'bo')
   plt.plot(epochs, validation_accuracies, 'ro')
@@ -54,23 +56,23 @@ def plot_loss_and_accuracy(history):
   plt.ylabel('Accuracy')
   
   plt.show()
-  sleep(10)
-  plt.clf()
+  sleep(5)
+  plt.close()
   
   
-int main():
+def main():
 
   from keras.datasets import reuters
 
   (train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
 
-  print("First sample of the Reuters training dataset:\n", train_data[0])
+  print("\n\n\nFirst sample of the Reuters training dataset:\n", train_data[0], "\n")
 
   word_index = reuters.get_word_index()
   inverse_word_index = dict((v, k) for k, v in word_index.iteritems())
 
   news_article_0 = ' '.join([inverse_word_index.get(i - 3, '?') for i in train_data[0]])
-  print("Corresponding news article:", news_article_0)
+  print("Corresponding news article:\n", news_article_0, "\n\n\n")
 
   train_data = vectorize(train_data)
   test_data = vectorize(test_data)
@@ -78,8 +80,8 @@ int main():
   train_labels = to_categorical(train_labels)
   test_labels = to_categorical(test_labels)
   
-  model = build_model(train_labels)
-  model.compile(optimizer=RMSProp(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+  model = build_model()
+  model.compile(optimizer=RMSprop(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
   
   validation_data = train_data[:1000]
   validation_labels = train_labels[:1000]
@@ -88,21 +90,22 @@ int main():
   partial_train_labels = train_labels[1000:]
   
   history = model.fit(partial_train_data, partial_train_labels, 
-                      epochs=20, batch_size=512,
+                      epochs=20, batch_size=512, verbose=0,
                       validation_data=(validation_data, validation_labels))
                       
   plot_loss_and_accuracy(history)
   
-  model = build_model(train_labels)
+  model = build_model()
   model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-  model.fit(train_data, train_labels, epochs=9, batch_size=512, validation_data=(test_data, test_labels))
+  model.fit(train_data, train_labels, epochs=9, batch_size=512, verbose=0,
+            validation_data=(test_data, test_labels))
   
   evaluation = model.evaluate(test_data, test_labels)
-  print("Loss on test data:{}\nAccuracy on test data: {}\n".format(evaluation))
+  print("\n\n\nLoss on test data:{evaluation[0]}\nAccuracy on test data: {evaluation[1]}\n".format(**locals()))
   
   predictions = model.predict(test_data)
-  print("Topic with highest probability for the first news article in the test dataset:\n", 
-        np.argmax(predictions[0]))
+  print("Topic with highest probability for the first news article in the test dataset:", 
+        np.argmax(predictions[0]), "\n")
   
 
 if __name__ == '__main__':
